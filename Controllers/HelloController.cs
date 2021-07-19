ecc44ecf3eecb4a0af7b4ef078f48dcf61828d07
bluephoenix5 <mia.a.lord@gmail.com>
2021-07-19 13:54:06 -0700

    Initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>" + "Hello World!" + "</h1>";
+            return Content(html, "text/html");
+        }
+
+        public IActionResult Welcome(string name)
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
