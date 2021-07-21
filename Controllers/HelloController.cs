c5788375181c6f00350ba73b36b157fd79c30cb4
bluephoenix5 <mia.a.lord@gmail.com>
2021-07-21 13:10:59 -0700

    Added select and options

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -12,16 +12,61 @@ namespace HelloASPDotNET.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method= 'post' action= '/helloworld/welcome'>" + "<input type= 'text' name= 'name' />" + "<input type= 'submit' value= 'Greet Me!' />" + "</form>";
-           
+            string html = "<form method= 'post' action= '/helloworld/language/'>" + 
+                "<input type= 'text' name= 'name' />" +  
+                "<select name='language' id='language'> <option value=''> Choose a language </option> <option value='english'> English </option> <option value='german'> German </option> <option value='french'> French </option> <option value='italian'> Italian </option> <option value='swedish'> Swedish </option>" + 
+                "</select>" + 
+                "<input type= 'submit' value= 'Greet Me!' />" + 
+                "</form>";
+        
             return Content(html, "text/html");
         }
 
         [HttpGet("welcome/{name?}")]
-        [HttpPost("welcom")]
+        [HttpPost("welcome")]
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+        
+
+        [HttpGet("language/")]
+        [HttpPost("language")]
+        public IActionResult CreateMessage(string language, string name)
+        {
+            string html = "";
+
+            if (language == "English")
+            {
+                html = $"<h1> Hello {name} </h1>";
+                return Content(html, "text/html");
+            }
+            else if (language == "German")
+            {
+                html = $"<h1> Hallo {name} </h1>";
+                return Content(html, "text/html");
+            }
+            else if (language == "French")
+            {
+                html = $"<h1> Bonjour {name} </h1>";
+                return Content(html, "text/html");
+            }
+            else if (language == "Italian")
+            {
+                html = $"<h1> Ciao {name} </h1>";
+                return Content(html, "text/html");
+            }
+            else if (language == "Swedish")
+            {
+                html = $"<h1> Hej {name} </h1>";
+                return Content(html, "text/html");
+            }
+            else
+            {
+                html = $"<h1> Sorry, that language is not supported </h1>";
+                return Content(html, "text/html");
+            }
+
         }
     }
 }
