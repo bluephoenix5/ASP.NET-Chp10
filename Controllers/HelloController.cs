d28313ecd414da902ab2080b82d7043d1466775b
bluephoenix5 <mia.a.lord@gmail.com>
2021-07-23 09:39:54 -0700

    Final commit

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -17,46 +17,8 @@ namespace HelloASPDotNET.Controllers
         [HttpPost("/hello")]
         public IActionResult Welcome(string name = "World")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-        }
-        
-
-        [HttpPost("hello/language")]
-        public IActionResult CreateMessage(string language, string name)
-        {
-            string html = "";
-
-            if (language == "English")
-            {
-                html = $"<h1> Hello {name} </h1>";
-                return Content(html, "text/html");
-            }
-            else if (language == "German")
-            {
-                html = $"<h1> Hallo {name} </h1>";
-                return Content(html, "text/html");
-            }
-            else if (language == "French")
-            {
-                html = $"<h1> Bonjour {name} </h1>";
-                return Content(html, "text/html");
-            }
-            else if (language == "Italian")
-            {
-                html = $"<h1> Ciao {name} </h1>";
-                return Content(html, "text/html");
-            }
-            else if (language == "Swedish")
-            {
-                html = $"<h1> Hej {name} </h1>";
-                return Content(html, "text/html");
-            }
-            else
-            {
-                html = $"<h1> Sorry, that language is not supported </h1>";
-                return Content(html, "text/html");
-            }
-
+            ViewBag.person = name;
+            return View();
         }
     }
 }
